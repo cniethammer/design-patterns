dc1f6931ffb2cd9258a3ae400ac8c45be6e20883
Christoph Niethammer <niethammer@hlrs.de>
2019-03-16 21:28:55 +0100

    Remove unnecessary get_do_output() method

--- c++/utils/Logger.h
+++ c++/utils/Logger.h
@@ -219,10 +219,6 @@ public:
 		return _do_output = val;
 	}
 
-	bool get_do_output() {
-		return _do_output;
-	}
-
 	/// initialize starting time
 	void init_starting_time() {
 #ifdef USE_GETTIMEOFDAY
