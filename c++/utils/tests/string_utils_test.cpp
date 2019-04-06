ad68874a9079f3fd7f659be482312d5382c3ff49
Christoph Niethammer <niethammer@hlrs.de>
2019-04-06 10:16:41 +0200

    Added string split function.

--- c++/utils/tests/string_utils_test.cpp
+++ c++/utils/tests/string_utils_test.cpp
@@ -30,3 +30,37 @@ TEST_CASE("string_to_type<string>(' Hello ') returns 'Hello'",
   REQUIRE(string_to_type<std::string>(" Hello ") == std::string("Hello"));
 }
 
+/* tests for split */
+TEST_CASE("split<string>(empty string, delimiter) returns ['']", "[split]") {
+  auto vec = split<std::string>("", "test");
+  REQUIRE(vec.size() == 1);
+  REQUIRE(vec[0] == std::string());
+}
+
+TEST_CASE("split<string>(str, str) returns ['', '']", "[split]") {
+  auto vec = split<std::string>("test", "test");
+  REQUIRE(vec.size() == 2);
+  REQUIRE(vec[0] == std::string());
+  REQUIRE(vec[1] == std::string());
+}
+
+TEST_CASE("split<string>(Hello World!) returns ['Hello', 'World']", "[split]") {
+  auto vec = split<std::string>("Hello World!");
+  REQUIRE(vec.size() == 2);
+  REQUIRE(vec[0] == std::string("Hello"));
+  REQUIRE(vec[1] == std::string("World!"));
+}
+
+TEST_CASE("split<int>(empty string, delimiter) returns [0]", "[split]") {
+  auto vec = split<int>("", ",");
+  REQUIRE(vec.size() == 1);
+  REQUIRE(vec[0] == 0);
+}
+
+TEST_CASE("split<int>(' 1,2,3 ',',') returns [1,2,3]", "[split]") {
+  auto vec = split<int>(" 1,2,3 ", ",");
+  REQUIRE(vec[0] == 1);
+  REQUIRE(vec[1] == 2);
+  REQUIRE(vec[2] == 3);
+  REQUIRE(vec.size() == 3);
+}
